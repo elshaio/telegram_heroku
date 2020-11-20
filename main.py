bfdaf3c187aeeb7e2943da71c0a7ff868598e6d5
Iván Román <develdkmd@outlook.com>
2020-11-20 07:39:31 -0700

    Changing appname to env variable

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ import json
 import os
 
 TOKEN = os.getenv('TOKEN', config.telegram_token)
+APP_NAME = os.getenv('HEROKU_APP_NAME')
 bot = Bot(TOKEN)
-URL = 'https://{herokuapp}.herokuapp.com/{token}'.format(herokuapp=config.heroku_app, token=TOKEN)
+URL = 'https://{herokuapp}.herokuapp.com/{token}'.format(herokuapp=APP_NAME, token=TOKEN)
 payload = {'url': URL}
 requests.get('https://api.telegram.org/bot{token}/setWebhook'.format(token=TOKEN), params=payload)
 
